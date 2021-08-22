f662fa3d57b1bbcef77f554666ac5a5f59206681
xjy2638468 <2205235778@qq.com>
2021-08-22 13:36:28 +0800

    index finish

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="recommend-title">热销推荐</div>
         <ul>
-          <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+          <li class="item border-bottom" v-for="item of list" :key="item.id">
               <img class="item-img" :src="item.imgUrl" />
             <div class="item-info">
               <p class="item-title">{{item.title}}</p>
@@ -17,40 +17,8 @@
 <script>
 export default {
   name: 'HomeRecommend',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
-        title: '故宫',
-        desc: '世界五大宫之首，穿越与您近在咫尺'
-      }, {
-        id: '0002',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
-        title: '北京野生动物园',
-        desc: '敢与森林之王近距离接触吗？'
-      }, {
-        id: '0003',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1602/22/22b6ddfc418d0c3b90.water.jpg_200x200_9d9dabdb.jpg',
-        title: '颐和园',
-        desc: '中国现存最大的皇家园林'
-      }, {
-        id: '0004',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1501/c2/c212a154649088c1.water.jpg_200x200_69d1df69.jpg',
-        title: '水立方嬉水乐园',
-        desc: '避暑乐园玩水胜地'
-      }, {
-        id: '0005',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_200x200_444cc7e8.jpg',
-        title: '圆明园',
-        desc: '百年皇家的珍宝,民族耻辱的见证'
-      }, {
-        id: '0006',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_200x200_8eb33f07.jpg',
-        title: '恭王府',
-        desc: '一座恭王府，半部清代史'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
