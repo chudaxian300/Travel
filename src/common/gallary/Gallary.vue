907b9b0da535ff67f9f2054697aa0298c0d534af
xjy2638468 <2205235778@qq.com>
2021-08-24 12:12:14 +0800

    finish banner

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container" @click="handleBannerClick">
+      <div class="wrapper">
+        <swiper :options="swiperOptions">
+            <swiper-slide v-for="(item, index) of imgs" :key="index">
+              <img class="gallary-img" :src="item" />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        // 图片底部 1/2 效果设置
+        paginationType: 'fraction',
+        // 当dom元素变化进行刷新,解决条件显示后宽度计算问题
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-image" src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" />
+      <div class="banner-info">
+        <div class="banner-title">故宫(AAAAA景区)</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span>12</div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+   </div>
+</template>
+
+<script>
+// 路径源于travel\build\webpack.base.conf.js第40行
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'Detail',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg', 'https://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow:hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-image
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      color: #fff
+      line-height: .6rem
+      // linear-gradient() 函数用于创建一个表示两种或多种颜色线性渐变的图片
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        padding: 0 .4rem
+        height: .32rem
+        line-height: .32rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        margin-top: .14rem
+        .banner-icon
+          font-size: .24rem
+</style>
